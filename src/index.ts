91e23e71bde8ddb70c3d837e7d4f61e78b5f5eff
Arun <arun11.kb@gmail.com>
2025-05-03 12:24:28 +0530

    cors options and allowed origin added

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ dotenv.config()
 import express from 'express';
 import cors from 'cors';
 import connectDb from './config/connectDb';
+import corsOptions from './config/corsOptions';
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || ''
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
