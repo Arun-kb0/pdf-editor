11d9744b2d0bada494e5856cd80ef103275f7510
Arun <arun11.kb@gmail.com>
2025-05-03 11:19:52 +0530

    init commit - basic webserver setup using repo pattern implemented

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config()
+import express from 'express';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
